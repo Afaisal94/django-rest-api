54c171c0baa5cdfafe03e191944d5c36275026d9
A Faisal <52657193+Afaisal94@users.noreply.github.com>
2022-04-12 11:48:10 +0700

    Initial Commit

--- api_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import CartItem
-
-admin.site.register(CartItem)
--- product/serializers.py
+++ product/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import CartItem
+from .models import Product
 
-class CartItemSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(max_length=200)
     product_price = serializers.FloatField()
-    product_quantity = serializers.IntegerField(required=False, default=1)
+    product_quantity = serializers.IntegerField()
 
     class Meta:
-        model = CartItem
+        model = Product
         fields = ('__all__')
